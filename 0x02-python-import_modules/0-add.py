02f962d8393fea4a2729366d73a9cca2a2b54220
Abafanamkhize <sfiso04@gmail.com>
2024-01-18 06:44:09 -0800

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    from absolute_path_to_add_0 import add  # Replace with the actual absolute path
+    from add_0 import add
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
-
